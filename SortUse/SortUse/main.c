e8679e5012ea7245183ca9d052eee3fcfd0195f5
zhangzhe <meiszhe1@163.com>
2019-03-03 11:13:48 +0800

    1，输入信息。

--- SortUse/SortUse/main.c
+++ SortUse/SortUse/main.c
@@ -9,7 +9,8 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    printf("i am testing.");
+    
     return 0;
 }
